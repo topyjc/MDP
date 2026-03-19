2921b94563c358b59329f16661a9c2961121258b
topyjc <topjc486@gmail.com>
2026-03-19 19:13:45 +0900

    mqtt 추가

--- src/main/java/com/mdp/server/mqtt/MqttStartupListener.java
+++ src/main/java/com/mdp/server/mqtt/MqttStartupListener.java
@@ -13,8 +13,12 @@ public class MqttStartupListener {
         this.mqttService = mqttService;
     }
 
+    /**
+     * Spring Boot 기동이 끝난 뒤 MQTT 연결 시작
+     */
     @EventListener(ApplicationReadyEvent.class)
     public void onReady() {
+        System.out.println("[MQTT] ApplicationReadyEvent fired");
         mqttService.connect();
     }
 }
