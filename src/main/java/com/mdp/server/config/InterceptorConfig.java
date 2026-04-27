ceeda2a396249a55e31a299a2145c9b24f1835b4
yuonpyo <kko20_s24096@gclass.ice.go.kr>
2026-04-27 10:43:01 +0900

    .

--- src/main/java/com/mdp/server/config/InterceptorConfig.java
+++ src/main/java/com/mdp/server/config/InterceptorConfig.java
@@ -16,8 +16,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(jwtInterceptor)
-                .addPathPatterns("/api/**") // 모든 api 검사
+                .addPathPatterns("/api/devices/**") // api 검사
+//              .addPathPatterns("/api/commit또는board/**) // 댓글 / 게시판 jWT 필요할 때 주석 빼셈
                 .excludePathPatterns("/api/auth/**") // 로그인, 회원가입은 제외
-                .excludePathPatterns("/api/alert/**");
+                .excludePathPatterns("/api/alert/**")
+                .excludePathPatterns("/api/device/**");
     }
 }
