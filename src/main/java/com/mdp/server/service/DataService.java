1771c4e092d6d8f749f552dada39d30014c7ac43
yuonpyo <kko20_s24096@gclass.ice.go.kr>
2026-06-29 09:53:22 +0900

    21

--- src/main/java/com/mdp/server/service/DataService.java
+++ src/main/java/com/mdp/server/service/DataService.java
@@ -26,11 +26,13 @@ public class DataService {
             String responseBody = dbServerClient.sendData(data);
 
             System.out.println("[DB 응답 원본] : " + responseBody);
+
             if (responseBody != null && !responseBody.isBlank()) {
                 JsonNode root = objectMapper.readTree(responseBody);
 
                 boolean isSuccess = root.path("data").path("data").path("success").asBoolean(false);
 
+                System.out.println(isSuccess);
                 return isSuccess;
             }
 
