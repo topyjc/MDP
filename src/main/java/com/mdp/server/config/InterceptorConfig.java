959797d4fbc1850d8f562817911cb4056f7e93ce
yuonpyo <kko20_s24096@gclass.ice.go.kr>
2026-07-08 21:06:13 +0900

    80

--- src/main/java/com/mdp/server/config/InterceptorConfig.java
+++ src/main/java/com/mdp/server/config/InterceptorConfig.java
@@ -20,6 +20,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/devices/**") // QR
                 .addPathPatterns("/api/private/boards/**") // 게시판 추가 & 좋아요 추가
                 .addPathPatterns("/api/private/comments") // 댓글 추가
+                .addPathPatterns("/api/auth/private/guardians/status-check") // 보호자-피보호자 조회
                 .excludePathPatterns("/api/auth/**") // 로그인, 회원가입 제외
                 .excludePathPatterns("/api/alert/**") // 알림 제외
                 .excludePathPatterns("/api/device/**"); // 센서 제어
